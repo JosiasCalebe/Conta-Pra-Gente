5bc19e47e5a129886133f3cc91562b4cd4f9d5bb
MathJOP7565 <matheus.psantos2016@gmail.com>
2019-10-08 17:24:15 -0300

    Home Chat finished

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 
-// Teste Header
+// Header
 import Header from '../../components/Header/index';
+
+// ChatModal
+import Chat from '../../components/ChatModal/index';
 
 // Imagens
 import SecImage from '../../components/SVG/svgHome';
-import Online from '../../components/SVG/online.png';
+import YouTubeLogo from '../../components/SVG/youtube.png';
+
 
 import './style.css'
 
@@ -36,29 +40,27 @@ export default function App() {
           </div>
           <button className="btn">Entre no Chat</button>
         </div>
-        <div className="chatCont">
-          <div className="infoBar">
-            <h3>Mediador <img src={Online} alt="Online icon"/></h3>
-              <div className="quit">
-                <div className="x"></div>
-                <div className="x"></div>
-              </div> 
-            </div>
-          <div className="messages">
-            <div className="welcomeChat">
-              <h3>Bem Vindo ao <span><a href="/#home">Conta Pra Gente</a></span>!</h3>
-            </div>
-            <div className="chatMessages">
-              <div className="userMessages">
-                
-              </div>
-              <div className="myMessages"></div>
-            </div>
+        <Chat />
+      </section>
+      <section className="sec3">
+        <div className="cont">
+          <h2>Mural</h2>
+          <div className="pCont">
+            O Mural tem depoimentos e frases motivacionais
           </div>
-          <div className="chatInput">
-            <div className="textChat">Escreva sua mensagem...</div>
-            <div className="btnChat">Enviar</div>
+          <button className="btn">Acesse o Mural</button>
+        </div>
+      </section>
+      <section className="sec4">
+        <div className="cont">
+          <h2>Vídeos</h2>
+          <div className="pCont">
+            Os Vídeos falaram sobre assuntos cotidianos
           </div>
+          <button className="btn">Veja os Vídeos</button>
+        </div>
+        <div className="video">
+          <img src={YouTubeLogo} alt="Logo do Youtube"/>
         </div>
       </section>
     </div >
