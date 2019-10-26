aa59a3c42c9e8ca6fee1a746c918872656afacea
Matheus Santos <matheus.psantos2016@gmail.com>
2019-10-26 13:38:16 -0300

    Link page all links added

--- frontend/src/pages/Links/index.js
+++ frontend/src/pages/Links/index.js
@@ -10,6 +10,102 @@ export default function App() {
     return (
         <div className="container">
             <Header />
+            <section className="link-main-sec">
+                <div class="link-title">
+                    <h3>Links Úteis</h3>
+                    <h4>Para conhecer mais sobre voluntariado, apoio emocional, prevenção do suicídio, saúde mental e outros temas afins, acesse também:</h4>
+                </div>
+                <div class="link-content">
+                    <div class="link-list-links">
+                        <h5>Portal Ministério da Saude (MS)</h5>
+                        <a href="http://portalms.saude.gov.br/saude-de-a-z/suicidio">http://portalms.saude.gov.br/saude-de-a-z/suicidio</a>
+                    </div>
+                    <div class="link-list-links">
+                        <h5>Centro de Atenção Psicosocial (CAPS)</h5>
+                        <a href="http://">http://portalms.saude.gov.br/saude-para-voce/saude-mental/acoes-e-programas-saude-mental/centro-de-atencao-psicossocial-caps</a>
+                    </div>
+                    <div class="link-list-links">
+                        <h5>Centro de Referência de Assistência Social (CRAS)</h5>
+                        <a href="http://">http://mds.gov.br/assuntos/assistencia-social/unidades-de-atendimento/cras</a>
+                    </div>
+                    <div class="link-list-links">
+                        <h5>Movimento Setembro Amarelo, Dia mundial de Prevenção ao Suicídio</h5>
+                        <a href="http://">http://www.setembroamarelo.org.br/</a>
+                    </div>
+                    <div class="link-list-links">
+                        <h5>Educação Emocional</h5>
+                        <a href="http://">http://www.amigosdozippy.org.br/</a>
+                    </div>
+                    <div class="link-list-links">
+                        <h5>Associação Brasileira de Psiquiatria</h5>
+                        <a href="http://"> http://www.abp.org.br/</a>
+                    </div>
+                    <div class="link-list-links">
+                        <h5>Movimento Conte Comigo, Prevenção a Depressão</h5>
+                        <a href="http://">http://www.contecomigo.org.br/</a>
+                    </div>
+                    <div class="link-list-links">
+                        <h5>Rede Brasiliera de Prevenção ao Suicídio</h5>
+                        <a href="http://">http://www.rebraps.com.br/</a>
+                    </div>
+                    <div class="link-list-links">
+                        <h5>Informações sobre prevenção do suicídio</h5>
+                        <a href="http://">www.prevencaosuicidio.blog.br</a>
+                    </div>
+                    <div class="link-list-links">
+                        <h5>Transtornos mentais e dependência química</h5>
+                        <a href="http://">http://www.franciscajulia.org.br</a>
+                    </div>
+                    <div class="link-list-links">
+                        <h5>Centro Voluntariado de São Paulo</h5>
+                        <a href="http://">http://www.centrovoluntariado.org.br</a>
+                    </div>
+                    <div class="link-list-links">
+                        <h5>Associação Brasileira de Estudos e Prevenção de Suicídios</h5>
+                        <a href="http://">http://www.abeps.org.br/</a>
+                    </div>
+                    <div class="link-list-links">
+                        <h5>Sociedade Portuguesa de Suicidologia</h5>
+                        <a href="http://">http://www.spsuicidologia.pt</a>
+                    </div>
+                    <div class="link-list-links">
+                        <h5>Samaritanos de Londres</h5>
+                        <a href="http://">http://www.samaritans.org</a>
+                    </div>
+                    <div class="link-list-links">
+                        <h5>Befrienders Worldwide</h5>
+                        <a href="http://">http://www.befrienders.org</a>
+                    </div>
+                    <div class="link-list-links">
+                        <h5>Associação Internacional para a Prevenção do Suicídio</h5>
+                        <a href="http://">http://www.iasp.info</a>
+                    </div>
+                    <div class="link-list-links">
+                        <h5>Associação Americana de Suicidologia</h5>
+                        <a href="http://">http://www.suicidology.org/</a>
+                    </div>
+                    <div class="link-list-links">
+                        <h5>Associação Canadense para a Prevenção do Suicídio</h5>
+                        <a href="http://">http://www.casp-acps.ca</a>
+                    </div>
+                    <div class="link-list-links">
+                        <h5>Telefono amigo-Italia</h5>
+                        <a href="http://">http://www.telefonoamigo.org</a>
+                    </div>
+                    <div class="link-list-links">
+                        <h5>Voz de apoio – Portugal</h5>
+                        <a href="http://">http://www.vozdeapoio.pt</a>
+                    </div>
+                    <div class="link-list-links">
+                        <h5>Telefone de Esperança – Portugal</h5>
+                        <a href="http://">http://www.telefonedaesperanca.pt</a>
+                    </div>
+                    <div class="link-list-links">
+                        <h5>Fundação Americana de Prevenção de Suicídio</h5>
+                        <a href="http://">https://afsp.org/</a>
+                    </div>                  
+                </div>
+            </section>
         </div >
     );
 }
